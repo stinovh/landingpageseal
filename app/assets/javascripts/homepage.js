fe2193e7679f932c28c3d81c42d8966ed555844c
Stijn <stijn.vanhaute@gmail.com>
2015-11-27 11:52:02 +0100

    scrolling

--- app/assets/javascripts/homepage.js
+++ app/assets/javascripts/homepage.js
@@ -10,7 +10,7 @@ $('#second-one').click(function(){
 
 
 $(function() {
-  $('a[href*=#]:not([href=#])').click(function() {
+  $('a[href*=#]:not([data-toggle="tab"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
       target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
